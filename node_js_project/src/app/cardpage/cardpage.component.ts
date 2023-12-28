faa178fec002a27402407eeac0ecfa798fca4f30
Omegalpha44 <charlesduval44@gmail.com>
2023-12-28 10:18:06 +0100

    Ajout du passage d'indice de l'url au script.hs

--- node_js_project/src/app/cardpage/cardpage.component.ts
+++ node_js_project/src/app/cardpage/cardpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cardpage',
@@ -6,9 +7,11 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
   styleUrls: ['./cardpage.component.css']
 })
 export class CardpageComponent {
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const indice = this.route.snapshot.queryParamMap.get('indice');
+    (window as any).indice = indice;
     const script = this.renderer.createElement('script');
     script.src = '../../assets/script.js'; // Replace with the actual path to the JavaScript file
     this.renderer.appendChild(document.body, script);
