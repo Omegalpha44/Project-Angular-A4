2f9d9a5688ab88ca6989f15f491296b26d8ddfa5
Rubiksel <ejderaksel@gmail.com>
2023-12-29 16:11:05 +0100

    debut mescours

--- node_js_project/src/app/mescours/mescours.component.ts
+++ node_js_project/src/app/mescours/mescours.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Renderer2} from "@angular/core";
 import {AgGridModule} from "ag-grid-angular";
-import {ColDef} from 'ag-grid-community';
+import {ColDef} from "ag-grid-community";
+
 
 
 @Component({
@@ -10,9 +11,9 @@ import {ColDef} from 'ag-grid-community';
   templateUrl: './mescours.component.html',
   styleUrls: ['./mescours.component.css']
 })
-export class MescoursComponent {
-  constructor(private renderer: Renderer2, private route: ActivatedRoute) { }
 
+export class MescoursComponent {
+  constructor(private renderer: Renderer2, private route: ActivatedRoute) {}
   ngOnInit() {
     const indice = this.route.snapshot.queryParamMap.get('indice');
     (window as any).indice = indice;
@@ -20,15 +21,16 @@ export class MescoursComponent {
     script.src = '../../assets/script.js'; // Replace with the actual path to the JavaScript file
     this.renderer.appendChild(document.body, script);
 
-    rowData: [
-        {created_at: "2021-01-01", description: "description 1", name: "name", project_id: 1}
-    ];
-
-    colDefs: [
-        {field: "created_at"},
-        {field: "description"},
-        {field: "name"},
-        {field: "project_id"}
-    ];
+    const rowData = [
+        {created_at: "2021-01-01", description: "description 1", name: "project 1", project_id: 1}
+    ]
+   const colDefs: ColDef[] = [
+       {field: "created_at"},
+       {field: "description"},
+       {field: "name"},
+       {field: "project_id"}
+   ]
   }
 }
+
+
